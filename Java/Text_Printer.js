cf275c007cbd8e0b8e2e079ef08c5ee6dd336921
TrentXWeb <50461232+TrentXWeb@users.noreply.github.com>
2019-05-12 01:27:27 -0700

    Version2

--- Java/Text_Printer.js
+++ Java/Text_Printer.js
@@ -3,10 +3,11 @@ trigger1 = true;
 
 class Printer{
 
-    constructor(str = "", id = "", i = 0){
+    constructor(str = "", id = "", i = 0, len = 0){
         this.list = str.split('');
         this.id = id;
         this.i = i;
+        this.len = len;
         this.done = false;
    
     }
@@ -18,12 +19,20 @@ class Printer{
     }
 
     printerComplete() {
-        console.log(this.i);
+        console.log(this.len);
         if (this.list[this.i] == ' ') { this.list[this.i] = '\u00A0'; }
-        if (this.list[this.i] == '-') { this.list[this.i] = '\n'; }
+        if (this.list[this.i] == '_') { this.list[this.i] = '\n'; }
+
+        if (this.len >= document.getElementById(this.id).clientWidth / 12 & this.list[this.i] == "\u00A0") {
+            document.getElementById(this.id).innerText += "\n";
+            this.len = 0;
+        }
+
+        document.getElementById(this.id).scrollWidth
         document.getElementById(this.id).innerText += this.list[this.i];
         if (this.i >= this.list.length - 1) { this.done = true; clearInterval(this.ints); }
         this.i += 1;
+        this.len += 1;
 
     }
 
